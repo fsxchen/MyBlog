f115308a734f3f1d1c73c9cf970e9fa91beb1645
fsxchen <fsxchen@gmail.com>
2015-09-14 09:52:01 +0800

    Ìí¼ÓÁËע²ácontrollerµÄһЩ²¿·Ö

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "GoWeb/server/docs"
 	_ "GoWeb/server/routers"
+    "GoWeb/server/controllers"
 
 	"github.com/astaxie/beego"
 )
@@ -12,5 +13,7 @@ func main() {
 		beego.DirectoryIndex = true
 		beego.StaticDir["/swagger"] = "swagger"
 	}
+    beego.RESTRouter("/object", &controllers.ObjectController{})
+    beego.RESTRouter("/user", &controllers.UserController{})
 	beego.Run()
 }
